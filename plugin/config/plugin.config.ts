004199b945237558e65154108f56881cec09a5ae
RepolloDev <96484170+RepolloDev@users.noreply.github.com>
2026-04-23 14:13:02 -0400

    feat: :sparkles: Update version to 1.0.7 and increase minimum app version to 0.22.0

--- plugin/config/plugin.config.ts
+++ plugin/config/plugin.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from '@config/utils/manifest';
 export default defineConfig({
     id: 'obsidian-plugin',
     name: 'Obsidian Plugin',
-    version: '1.0.6',
+    version: '1.0.7',
     description: 'A plugin for Obsidian',
     author: 'RepolloDev',
     authorUrl: 'https://github.com/RepolloDev',
     fundingUrl: 'https://github.com/sponsors/RepolloDev',
-    minAppVersion: '0.20.0',
+    minAppVersion: '0.22.0',
     isDesktopOnly: false,
 });
