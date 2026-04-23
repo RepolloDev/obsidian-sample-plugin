a07769a500ab7bd13852ac279abb78f3200eae79
RepolloDev <96484170+RepolloDev@users.noreply.github.com>
2026-04-23 14:10:08 -0400

    feat: :sparkles: Update version to 1.0.6 in plugin configuration

--- plugin/config/plugin.config.ts
+++ plugin/config/plugin.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@config/utils/manifest';
 export default defineConfig({
     id: 'obsidian-plugin',
     name: 'Obsidian Plugin',
-    version: '1.0.5',
+    version: '1.0.6',
     description: 'A plugin for Obsidian',
     author: 'RepolloDev',
     authorUrl: 'https://github.com/RepolloDev',
